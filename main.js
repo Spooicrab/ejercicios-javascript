3e4a5d7ce0a7728531801973274c38b501101203
Gabriel Ocampos <gabriel1234511@hotmail.com>
2023-05-22 17:30:37 -0300

    ejercicio 2 hecho

--- main.js
+++ main.js
@@ -43,6 +43,19 @@ console.log(calcularImpuestos(30, 500)); // 0
 ```
  */
 
+/* function calcularImpuestos(edad, ingresos) {
+  if (edad >= 18 && ingresos >= 1000) {
+    return ingresos * 0.4;
+  } else {
+    return 0;
+  }
+}
+
+console.log(calcularImpuestos(18, 1000)); // 400
+console.log(calcularImpuestos(40, 10000)); // 4000
+console.log(calcularImpuestos(17, 5000)); // 0
+console.log(calcularImpuestos(30, 500)); // 0
+ */
 //  //  //  //  //  //  //  //  //  //  //  //  //  //  //  //  //
 
 /* ## 3. IMC (ïndice de masa corporal)
